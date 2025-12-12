7ad8d75394e24e243737d6df01607e29ffd6d131
Matthew Gradidge <matt.gradidge@gmail.com>
2025-12-12 12:11:26 -0700

    Add App.tsx and main.jsx, this commit will not work as it is missing
    dataSource.ts

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useState, useMemo, useCallback } from 'react'
+import { AllCommunityModule, ModuleRegistry, InfiniteRowModelModule, ValidationModule, ClientSideRowModelModule, ColDef } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import { formatDistanceToNowStrict } from 'date-fns';
+import { GridReadyEvent, IGetRowsParams } from 'ag-grid-community/types/events';
+
+export type IRow = {
+	id: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	city: string;
+	registered_at: string;
+	full_name: string;
+	dsr: string;
+}
+
+ModuleRegistry.registerModules([
+	ClientSideRowModelModule,
+  InfiniteRowModelModule,
+	ValidationModule,
+	AllCommunityModule
+]);
+
+
+function App({ dataSource })  {
+	// Column Definitions: Defines & controls grid columns.
+	const [colDefs, _] = useState<ColDef<IRow>[]>([
+		{ field: 'id', editable: true, filter: true },
+		{ field: 'first_name', headerName: 'First Name', editable: true },
+		{ field: 'last_name', headerName: 'Last Name', editable: true },
+		{ field: 'email', headerName: 'Email', editable: true },
+		{ field: 'city' },
+		{ field: 'registered_at', headerName: 'Registered Date', editable: true, filter: 'agDateColumnFilter' },
+		{ field: 'full_name', headerName: 'Full Name', valueGetter: (params) => { if (params.data) return `${params.data.first_name} ${params.data.last_name}` } },
+		{ field: 'dsr', headerName: 'DSR', valueGetter: (params) => { if (params.data) return formatDistanceToNowStrict(new Date(params.data.registered_at), { addSuffix: true, unit: 'day' }) } },
+	]);
+
+	const onGridReady = useCallback((params: GridReadyEvent) => {
+		const tableDataSource = {
+			rowCount: 500,
+			getRows: async function(rowParams: IGetRowsParams) {
+				console.log('Requesting rows: ', rowParams.startRow, 'to', rowParams.endRow);
+				await dataSource(rowParams)
+			}
+		};
+		params.api.setGridOption('datasource', tableDataSource);
+	}, []);
+
+
+	const defaultColDef = useMemo(() => {
+		return {
+			flex: 1
+		};
+	}, []);
+
+	// Container: Defines the grid's theme & dimensions.
+	return (
+		<>
+			<div style={{ width: "100%", height: '100vh' }}>
+				<AgGridReact
+					columnDefs={colDefs as any}
+					defaultColDef={defaultColDef}
+					rowBuffer={0}
+					cacheBlockSize={100}
+          cacheOverflowSize={2}
+          maxConcurrentDatasourceRequests={1}
+          infiniteInitialRowCount={100}
+          maxBlocksInCache={10}
+					rowModelType={"infinite"}
+					onGridReady={onGridReady}
+				/>
+			</div>
+		</>
+	);
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App  from 'src/App.jsx'
+import { dataSource , createRandomUser } from 'src/dataSource'
+
+
+// Populate the fake data source with some initial data
+// This would normally come from a server
+const db = Array.from({ length: 500 }, () => createRandomUser())
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App dataSource={dataSource.bind(this, db)} />
+  </StrictMode>,
+)
