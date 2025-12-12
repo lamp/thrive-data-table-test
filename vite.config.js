f2cdc782c9311cab76b761c3c7471ee15f3f489c
Matthew Gradidge <matt.gradidge@gmail.com>
2025-12-12 12:09:19 -0700

    Base vite application scaffold

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+	resolve: {
+		alias: {
+			src: '/src',
+		},
+	},
+  plugins: [react()],
+	test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
+})
